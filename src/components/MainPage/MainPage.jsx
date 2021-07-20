542526afea48f0aadee247b6e5b847047f903cd6
Filip <Karguel1995@gmail.com>
2021-07-20 11:02:58 +0200

    add: animations, hover on photos

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,23 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import './MainPage.scss'
 import AboutUs from "./subcomponents/AboutUs/AboutUs";
 import Contact from "./subcomponents/Contact/Contact";
 
-import mainPhoto from '../../../images/mainPhotos/furnitures.jpg'
-import rightImage from '../../../images/mainPhotos/stairs.jpg'
+import mainPhoto from '../../../images/mainPhotos/gardenfurniture.jpg'
+import rightImageBefore from '../../../images/mainPhotos/stairsBefore.jpg'
+import rightImageAfter from '../../../images/mainPhotos/stairsAfter.jpg'
 import bottomImage from '../../../images/mainPhotos/furnitures2.jpg'
 
+
+
 const MainPage = () => {
+
+  const [stairsImage, setStairsImage] = useState(rightImageBefore)
+
   return (
     <main>
-      <div className="home-page">
-        <h1>Pyrcrafts - Pracownia stolarska</h1>
+      {/* <h1>Pyrcrafts - Pracownia stolarska</h1> */}
+      <div className="images-container">
         <div className="main-images-container">
-          <img src={mainPhoto} className="mainImage" />
-          <img src={rightImage} className="rightImage" />
+          <div className="image-container">
+            <h1>Meble ogrodowe</h1>
+            <div className="line"></div>
+            <img src={mainPhoto} className="left-image" />
+            <p>Poszukujesz dobrej jakości mebli ogrodowych, które przetrwają każde warunki?</p>
+          </div>
+
+          <div
+            className="image-container"
+            onMouseOver={() => { setStairsImage(rightImageAfter) }}
+            onMouseOut={() => { setStairsImage(rightImageBefore) }}
+          >
+            <h1>Schody</h1>
+            <div className="line"></div>
+            <img
+              src={stairsImage}
+              className="right-image" />
+            <p>Marzą Ci się nowoczesne schody z oświetleniem?</p>
+          </div>
         </div>
-        <div className="bottom-image-container">
+
+        <div className="image-container">
+          <h1>Meble</h1>
           <img src={bottomImage} className="bottomImage" />
         </div>
       </div>
--- src/components/MainPage/subcomponents/AboutUs/AboutUs.jsx
+++ src/components/MainPage/subcomponents/AboutUs/AboutUs.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Aos from 'aos';
+import "aos/dist/aos.css"
 
 import './AboutUs.scss'
 
@@ -7,37 +9,44 @@ import quality from '../../../../../images/skills/quality.png'
 import services from '../../../../../images/skills/services.png'
 
 const AboutUs = () => {
+  useEffect(() => {
+    Aos.init({
+      duration: 2000,
+      mirror: true
+    });
+  }, []);
   return (
     <div className="about-us-page">
       <div className="advantages">
-        <h2>Dlaczego warto wybrać akurat nas?</h2>
+        <h2 data-aos="slide-right">Dlaczego warto wybrać akurat nas?</h2>
         <div className="advantages-list">
-          <div>
+          <div >
             <p>Doświadczenie</p>
-            <img src={experience} />
+            <img data-aos="zoom-in" src={experience} />
           </div>
-          <div>
+          <div >
             <p>Sprawdzone materiały</p>
-            <img src={quality} />
+            <img data-aos="zoom-in" data-aos-delay="500" src={quality} />
           </div>
-          <div>
+          <div >
             <p>Szeroki wachlarz usług</p>
-            <img src={services} />
+            <img data-aos="zoom-in" data-aos-delay="1000" src={services} />
           </div>
         </div>
       </div>
       <div className="about-us-container">
         <div className='about-us-container-left'>
           <h2>O nas</h2>
-          <p>Jesteśmy dumni z tego, że zapewniamy wysokiej jakości, a jednocześnie wydajną i skuteczną obsługę każdemu z naszych klientów, wykorzystując naszą zaawansowaną wiedzę, umiejętności i doświadczenie, aby upewnić się, że każda praca jest wykonywana w najlepszym możliwym standardzie oraz w przystępnych cenach.</p>
+          <p data-aos="fade-up">Jesteśmy dumni z tego, że zapewniamy wysokiej jakości, a jednocześnie wydajną i skuteczną obsługę każdemu z naszych klientów, wykorzystując naszą zaawansowaną wiedzę, umiejętności i doświadczenie, aby upewnić się, że każda praca jest wykonywana w najlepszym możliwym standardzie oraz w przystępnych cenach.</p>
         </div>
         <div className='about-us-container-right'>
           <h2>Nasze usługi</h2>
           <ul>
-            <li><i class="far fa-check-square"></i> Meble</li>
-            <li><i class="far fa-check-square"></i> Schody</li>
-            <li><i class="far fa-check-square"></i> Drzwi</li>
-            <li><i class="far fa-check-square"></i> Okna</li>
+            <li data-aos="fade-left"><i className="far fa-check-square"></i> Meble</li>
+            <li data-aos="fade-left"><i className="far fa-check-square"></i> Schody</li>
+            <li data-aos="fade-left"><i className="far fa-check-square"></i> Drzwi</li>
+            <li data-aos="fade-left"><i className="far fa-check-square"></i> Okna</li>
+            <li data-aos="fade-left"><i className="far fa-check-square"></i> Meble ogrodowe</li>
           </ul>
         </div>
       </div>
